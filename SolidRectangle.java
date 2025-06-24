ddbd0a452716e6caf0dae2310b1224bd9535fc15
SiddhiM4 <siddhimandekar47@gmail.com>
2025-06-24 14:19:02 +0530

    dynamic solid rect pattern

--- SolidRectangle.java
+++ SolidRectangle.java
@@ -6,7 +6,7 @@ public class SolidRectangle{
         int n = sc.nextInt();
 
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n+1; j++){
+            for(int j = 1; j <= n+1 ; j++){
                 System.out.print("*");
             }
             System.out.println();
